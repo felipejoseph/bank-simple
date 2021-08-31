0da0da9f6d934c8c4caccbc6671e2f710ae93520
Felipe-Joseph <felipejoseph45@hotmail.com>
2021-08-30 21:50:34 -0300

    0.0.2

--- src/Conta.java
+++ src/Conta.java
@@ -1,9 +1,29 @@
 
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
+	private double saldo;
+	private int agencia;
+	private int numero;
 	String titular;
+	
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 	
 	
 	
--- src/checkbank.java
+++ src/checkbank.java
@@ -3,33 +3,78 @@ import java.util.Scanner;
 public class checkbank {
 	static Scanner s = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Bem vindo ao Check Bank");
-		System.out.println("Selecione a seguir alguma das opções");
-		System.out.println("1 - Abertura de conta");
-		System.out.print("");
-		System.out.println("2 - Sacar");
-		System.out.print("");
-		System.out.println("3 - Depositar ");
-		System.out.print("");
-		System.out.println("4 - Transferir ");
-		
-		int opcao = s.nextInt();
-	
 
-		if(opcao == 1) {
-			System.out.println("teste de if");
-			
-		}else if (opcao == 2) {
-			System.out.println("opcao 2");
-		}else if (opcao == 3) {
-			System.out.println("opcao 3");
-		}else if (opcao ==4) {
-			System.out.println("opcao 4");
-		}else {
-			System.out.println("Opção invalida ");
+	public static void main(String[] args) throws InterruptedException {
+		Boolean isRunning = Boolean.TRUE;
+		
+		
+			System.out.println("Bem vindo ao Check Bank");
+			while (isRunning) {
+			System.out.println("Selecione a seguir alguma das opções");
+			System.out.println("1 - Abertura de conta");
+			System.out.print("");
+			System.out.println("2 - Sacar");
+			System.out.print("");
+			System.out.println("3 - Depositar ");
+			System.out.print("");
+			System.out.println("4 - Transferir ");
+
+			int opcao = s.nextInt();
+			Conta primeiraconta = new Conta();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("Digite o seu nome");
+				String titular = s.next();
+				System.out.println("ola " + titular + " sua conta foi criada corretamente ");
+
+				System.out.println("Deseja colocar algum valor ? 1-sim / 2-nao");
+
+				int resposta = s.nextInt();
+
+				if (resposta == 1) {
+					System.out.println("por favor digite o valor desejado");
+					double valor = s.nextDouble();
+					primeiraconta.setSaldo(primeiraconta.getSaldo() + valor);
+					System.out.println("Parabens pelo Deposito");
+
+				} else {
+					System.out.println("Ok");
+					
+				}
+
+				System.out.println("Seu saldo atual é de R$ " + primeiraconta.getSaldo());
+				System.out.println("");
+				System.out.println("Deseja retornar ao menu ?");
+				System.out.println("1-sim / 2-nao");
+				int resposta2 = s.nextInt();
+				if(resposta2 == 1) {
+					
+					System.out.println("Carregando...");
+					Thread.sleep(3000);
+					isRunning = Boolean.TRUE;
+					System.out.println("");
+				}else {
+					System.out.println("até logo");
+					isRunning = Boolean.FALSE;
+				}
+				
+				break;
+
+			case 2:
+				System.out.println("opcao 2");
+				System.out.println("olá qual o valor você deseja sacar ? ");
+
+				break;
+			case 3:
+				System.out.println("opcao 3");
+				break;
+			case 4:
+				System.out.println("opcao 4");
+				break;
+			default:
+				System.out.println("Opção invalida ");
+			}
 		}
-}}
-
+	}
+}
